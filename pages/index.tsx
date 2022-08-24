ebba2de62ecf5381306ec770da5cab49fa0754e8
skyeome <tjru7230@naver.com>
2022-08-24 22:33:08 +0900

    #0.0 NextJS & Tailwind 설치

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-blue-500">wow.. it works?</h1>
+    </div>
+  );
+};
+
+export default Home;
