1d573b0aba078aae02eee89fffb6d76b6c239050
skyeome <tjru7230@naver.com>
2022-11-19 16:36:42 +0900

    #1.6 all pages styling & components end

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -1,9 +1,13 @@
 import type { NextPage } from "next";
+import Button from "../../components/button";
+import Input from "../../components/input";
+import Layout from "../../components/layout";
+import TextArea from "../../components/textarea";
 
 const Upload: NextPage = () => {
   return (
-    <div className="px-4 py-16">
-      <div>
+    <Layout canGoBack title="Upload Product">
+      <form className="p-4 space-y-4">
         <div>
           <label className="w-full cursor-pointer text-gray-600 hover:border-orange-500 hover:text-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 h-48 rounded-md">
             <svg
@@ -20,43 +24,22 @@ const Upload: NextPage = () => {
                 strokeLinejoin="round"
               />
             </svg>
-
             <input className="hidden" type="file" />
           </label>
         </div>
-      </div>
-      <div className="my-5">
-        <label htmlFor="price" className="text-sm font-bold text-gray-700">
-          가격
-        </label>
-        <div className="rouned-md relative flex items-center shadow-md">
-          <div className="absolute left-0 pointer-events-none pl-3 flex items-center justify-center">
-            <span className="text-gray-500 text-sm">₩</span>
-          </div>
-          <input
-            id="price"
-            className="appearance-none pl-7 w-full px-3 py-2 border border-gray-300 rounded-r-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
-            type="text"
-            placeholder="0"
-          />
-          <div className="absolute right-0 pointer-events-none pr-3 flex items-center">
-            <span className="text-gray-500">원</span>
-          </div>
-        </div>
-      </div>
-      <div>
-        <label className="text-sm font-bold text-gray-700">제품 설명</label>
-        <div>
-          <textarea
-            className="mt-1 resize-none shadow-sm w-full focus:ring-1 focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500"
-            rows={4}
-          />
-        </div>
-      </div>
-      <button className="mt-5 w-full bg-orange-500 hover:bg-orange-600 text-white py-3 px-4 border border-transparent rounded-md shadow-sm text-md font-bold focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">
-        Upload product
-      </button>
-    </div>
+        <Input required label="Name" name="name" type="text" />
+        <Input
+          required
+          label="Price"
+          placeholder="0.00"
+          name="price"
+          type="text"
+          kind="price"
+        />
+        <TextArea name="description" label="Description" />
+        <Button text="Upload item" />
+      </form>
+    </Layout>
   );
 };
 
